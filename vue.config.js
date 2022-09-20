f1a3f64468ac9ef9091d57e100d3eb9cd0e65df1
Григорий <mikigrigorij@yandex.ru>
2022-09-20 16:00:44 +0300

    build

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/portfolio/',
-=======
   lintOnSave: false,
   publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
   pluginOptions: {
@@ -10,5 +7,4 @@ module.exports = {
       patterns: ["E:\\prog\\portfolio\\src\\assets\\style\\modules\\colors.scss"],
     },
   },
->>>>>>> adb4bd8024eec869e2e9ca42597e099baacb9c2d
 };
